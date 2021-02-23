7166a6bab579ee6b80fd77b41fe824f2695ccc34
Elizabeth Lola <leezeelola@gmail.com>
2021-02-23 23:25:06 +0100

    changed design

--- components/Post.vue
+++ components/Post.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <article class="bg-gray-800 p-5 my-3 flex justify-between items-center rounded">
+      <article class="bg-gray-800 p-5 my-3 md:flex justify-between items-center rounded">
         <div class="">
           <div class="text-gray-500 text-2xl my-2">{{author}}</div>
             <a
@@ -12,13 +12,13 @@
             {{title}}
           </a>
           
-          <div class="flex mt-5">
+          <div class="flex mt-5 text-xs">
             <div class="bg-gray-700 p-2 mx-1">total comments: {{comments_count}}</div>
             <div class="bg-gray-700 p-2 mx-1">points: {{points}}</div>
             <div class="bg-gray-700 p-2 mx-1">Posted {{time_ago}}</div>
           </div>
         </div>
-          <nuxt-link :to="'/post/' + id" class="py-2 px-5 border border-gray-700 rounded">
+          <nuxt-link :to="'/post/' + id" class="py-2 px-5 border border-gray-700 rounded block  text-center my-3 md:my-0">
             View
           </nuxt-link>
       </article>
